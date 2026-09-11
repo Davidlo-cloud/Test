0a1bcbe327508c8a6770f33f0500da31f2f88aaf
DavidLisOlivebring <david.lisolivebring@gmail.com>
2026-09-11 09:39:47 +0200

    Name

--- Program.cs
+++ Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main()
     {
         System.Console.WriteLine("Hello Git!");
+        System.Console.WriteLine(DoSomething("David"));
+    }
+    static string DoSomething(string name)
+    {
+        return $"Hello {name}";
     }
 }
