374ac24c678c52a7a7a6d552a3ffdfdfd3b2fe12
DavidLisOlivebring <david.lisolivebring@gmail.com>
2026-09-14 09:25:53 +0200

    Made a bad decision

--- Account.cs
+++ Account.cs
@@ -9,5 +9,6 @@ public class Account
     {
         System.Console.WriteLine("Nisse was here!");
         System.Console.WriteLine("Nisse did a bad choice!");
+        System.Console.WriteLine("Oooops inte bra");
     }
 }
